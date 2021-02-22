4408b1ab952a84e7f89fdd4586861e0490893d1e
AgusBaez <agustinbaezignacio@gmail.com>
2021-02-22 19:35:02 -0300

    update database conn

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-    $server = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'custom';
+    $server = 'us-cdbr-east-03.cleardb.com';
+    $username = 'b61c0add977fa2';
+    $password = '315075e8';
+    $database = 'heroku_9a1be12fa54d8f9';
 
     try {
         $conn = new PDO("mysql:host=$server;dbname=$database;" , $username , $password);
